d85b3294210802e2a032bd6331ccc9b58a41a584
Marko Stojanac <m.stojanac@vegait.rs>
2023-10-02 11:38:48 +0200

    Add header structure

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,8 +1,32 @@
 <template>
-    <header>
-        <div class="bg-black">
-            <img :src="LogoImg" alt="dept logo image">
+    <header class="flex justify-between bg-black px-20 py-10">
+        <div>
+            <a href="/"><img :src="LogoImg" alt="dept logo image"></a>
         </div>
+
+        <nav>
+            <ul class="flex uppercase text-white">
+                <li class="px-6">
+                    <a href="javascript:;">Work</a>
+                </li>
+                <li class="px-6">
+                    <a href="javascript:;">Culture</a>
+                </li>
+                <li class="px-6">
+                    <a href="javascript:;">Services</a>
+                </li>
+                <li class="px-6">
+                    <a href="javascript:;">Insights</a>
+                </li>
+                <li class="px-6">
+                    <a href="javascript:;">Careers</a>
+                </li>
+                <li class="px-6">
+                    <a href="javascript:;">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
     </header>
 </template>
 
