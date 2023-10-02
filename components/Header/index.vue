711aa39df10c10ee1da4ba55a47b4492d237f918
Marko Stojanac <m.stojanac@vegait.rs>
2023-10-02 11:19:51 +0200

    Set router and add global layout

---
+++ components/Header/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div class="bg-black">
+            <img :src="LogoImg" alt="dept logo image">
+        </div>
+    </header>
+</template>
+
+<script setup>
+import LogoImg from '@/assets/logos/dept-logo-white.svg'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Header />
+        <slot/>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
